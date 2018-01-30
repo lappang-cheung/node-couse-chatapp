35a02f7cb4cf0800677eb366d98f807aa3994ffd
Lemonjai <leocheung_1986@hotmail.com>
2018-01-30 17:07:11 -0500

    adding geocloation

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,19 @@ jQuery('#message-form').on('submit', function(e){
 
   });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+  if(!navigator){
+    return alert('Geolocation not support on your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function(){
+    alert('Unable to fetch location');
+  });
+});
--- server/server.js
+++ server/server.js
@@ -25,6 +25,14 @@ io.on('connection', (socket) => {
     callback('This is from the server');
   });
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newMessage', generateMessage('Admin', `${coords.latitude},${coords.longitude}`));
+  });
+
+  socket.on('disconnect', () =>{
+    console.log('User was disconnected');
+  });
+
 });
 
 server.listen(port, () => {
