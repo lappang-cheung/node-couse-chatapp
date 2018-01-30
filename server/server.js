ed6fe0a470ae47061b208be6ee21f2f0aa99bb21
Lemonjai <leocheung_1986@hotmail.com>
2018-01-30 15:24:13 -0500

    skeleton chatapp

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage',{
+    from: 'mike@example.com',
+    text: 'Hey. what going on?',
+    createAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
 });
 
 server.listen(port, () => {
