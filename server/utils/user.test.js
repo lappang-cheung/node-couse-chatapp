6ff79977e101f80ef40c0a468e853a43574ca5be
Lemonjai <leocheung_1986@hotmail.com>
2018-01-30 23:13:15 -0500

    added the users to store users

---
+++ server/utils/user.test.js
@@ -0,0 +1,20 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+
+  it('should add new user', () => {
+    var users = new Users();
+    var user ={
+      id: '123',
+      name: 'Leo',
+      room: 'The Office'
+    };
+    var resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+
+  });
+
+});
