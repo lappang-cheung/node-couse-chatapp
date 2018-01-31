1a1ea70f67a576a5a92ffd3c598fcd63d4948456
Lemonjai <leocheung_1986@hotmail.com>
2018-01-30 21:25:30 -0500

    fix some issues with url

--- public/js/index.js
+++ public/js/index.js
@@ -23,14 +23,25 @@ socket.on('newMessage', function (message) {
 });
 
 socket.on('newLocationMessage', function (message) {
+
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#location-message-template').html();
-  var html = Mustache.render(template,{
+  var html = Mustache.render(template, {
     from: message.from,
     url: message.url,
     createdAt: formattedTime
   });
-  jQuery('#location-message-template').append(html);
+
+  jQuery('#messages').append(html);
+
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
